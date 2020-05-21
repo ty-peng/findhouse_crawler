ea0b23aff73ecf826f69b951d4dc728349993851
ty-peng <tiany.dev@qq.com>
2020-05-21 16:44:32 +0800

    feat: mongodb support in pipelines

--- findhouse_crawler/pipelines.py
+++ findhouse_crawler/pipelines.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import pymongo
+from findhouse_crawler.settings import MONGO_HOST, MONGO_PORT, MONGO_DB_NAME, MONGO_DB_COLLECTION
+
 
 # Define your item pipelines here
 #
@@ -7,5 +10,16 @@
 
 
 class FindhouseCrawlerPipeline(object):
+    def __init__(self):
+        host = MONGO_HOST
+        port = MONGO_PORT
+        db_name = MONGO_DB_NAME
+        table_name = MONGO_DB_COLLECTION
+        client = pymongo.MongoClient(host=host, port=port)
+        db = client[db_name]
+        self.post = db[table_name]
+
     def process_item(self, item, spider):
+        data = dict(item)
+        self.post.insert(data)
         return item
